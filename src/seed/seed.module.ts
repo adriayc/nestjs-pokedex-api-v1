7625727466aa467d09c4e03ff3179566c9b8262b
Adriano Ayala <adriano.ayc@gmail.com>
2026-04-13 00:05:22 -0400

    Insertar Pokemons por lote

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [
+    // Import module to use it (PokemonModule)
+    PokemonModule,
+  ],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const { data } = await axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
     // Format the data
-    data.results.forEach(({ name, url }) => {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      await this.pokemonModel.create({ name, no });
     });
 
-    return data.results;
+    return 'Seed executed!';
   }
 }
